3f524010b581b491a779ab4715ddeb9cb58c41e1
Luis Rivas <luisfrm_@outlook.com>
2026-02-08 04:27:09 -0400

    initial config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
