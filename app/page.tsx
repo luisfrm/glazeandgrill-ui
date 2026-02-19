41e9319344269d238443b10b3a9941c7560d167a
Luis Rivas <luisfrm_@outlook.com>
2026-02-19 18:46:55 -0400

    cn on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Categories from '@/components/Categories';
 import Hero from '@/components/Hero';
 import Menu from '@/components/Menu';
+import { cn } from '@/helper/helper';
 
 export default function Home() {
   return (
-    <section className='min-h-screen bg-cream'>
+    <section className={cn('min-h-screen bg-cream')}>
       <main className="max-w-5xl flex flex-col mx-auto px-6 pt-28 pb-8 space-y-12">
         <Hero />
         <Categories />
