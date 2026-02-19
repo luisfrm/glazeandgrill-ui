d9ad1791b8d48bbbe28065394ae14671e4af0486
Luis Rivas <luisfrm_@outlook.com>
2026-02-19 18:46:31 -0400

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
-   images: {
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'lh3.googleusercontent.com',
         pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: 'foodish-api.com',
         pathname: '/**',
       },
     ],
